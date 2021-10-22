8e5c4b1fa31d5bf12ee96de45aea2c77664455f1
Maurice Meyer <mmeyer@anaconda.com>
2021-10-22 15:46:56 +0200

    add section to test required_version fault

--- terrific/backend.tf
+++ terrific/backend.tf
@@ -1,7 +1,13 @@
 terraform {
   required_version = "1.0.0"
-
-  invalid_config = {
-    test = "hello"
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.26.1"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "2.24.1"
+    }
   }
 }

c40028caa13a8b9d28b8d15becc03c1ba7bf6e49
Maurice Meyer <mmeyer@anaconda.com>
2022-08-10 12:07:08 +0200

    chore: clean up and add conda environment.yml

--- workspaces/awesome.tf
+++
@@ -1,7 +0,0 @@
-resource "tfe_workspace" "awesome" {
-  name              = "awesome"
-  organization      = "morremeyer"
-
-  # renovate
-  terraform_version = "1.0.6"
-}
--- workspaces/terrific.tf
+++
@@ -1,7 +0,0 @@
-resource "tfe_workspace" "terrific" {
-  name              = "terrific"
-  organization      = "morremeyer"
-
-  # renovate
-  terraform_version = "1.0.5"
-}
